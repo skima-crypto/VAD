8645724b16e27ca952436c2abbf7319ae8f5df0a
Crypto Evangelist <139472454+skima-crypto@users.noreply.github.com>
2025-11-22 11:46:52 +0000

    .

--- my-mobile-app/app.config.js
+++ my-mobile-app/app.config.js
@@ -6,40 +6,43 @@ export default {
     slug: "my-mobile-app",
     version: "1.0.0",
     orientation: "portrait",
+
+    cli: {
+      appVersionSource: "remote",   // <--- Fixes the warning
+    },
+
     icon: "./assets/icon.png",
     splash: {
       image: "./assets/splash-icon.png",
       resizeMode: "contain",
       backgroundColor: "#ffffff",
     },
-    platforms: ["ios", "android", "web"],
-
-    android: {
-      package: "com.skima.vadapp",  // <<< ADD THIS
-      versionCode: 1
-    },
+    assetBundlePatterns: ["**/*"],
 
     ios: {
-      bundleIdentifier: "com.skima.vadapp" // optional but recommended
+      bundleIdentifier: "com.mine.vadapp",
+      config: {
+        googleMobileAdsAppId: process.env.IOS_ADMOB_APP_ID,
+      },
+    },
+    android: {
+      package: "com.mine.vadapp",
+      versionCode: 1,
+      config: {
+        googleMobileAdsAppId: process.env.ANDROID_ADMOB_APP_ID,
+      },
     },
 
-    plugins: [
-      [
-        "expo-ads-admob",
-        {
-          androidAppId: process.env.ANDROID_ADMOB_APP_ID,
-          iosAppId: process.env.IOS_ADMOB_APP_ID,
-        },
-      ],
-    ],
+    plugins: ["react-native-google-mobile-ads"],
 
     extra: {
       SUPABASE_URL: process.env.SUPABASE_URL,
       SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
       SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
+
       eas: {
-        projectId: "your-project-id-here"
-      }
+        projectId: "01e6250a-1ffa-4f6d-839c-940a486d0f3c",
+      },
     },
   },
 };
